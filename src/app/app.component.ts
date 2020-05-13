74fea3f6d77b57e4cdac5fe83d668605cd8f0a48
Cristian Alvarez <c.alvarez.r3004@gmail.com>
2020-05-13 18:41:32 -0400

    People list using ngFor directive

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
 import { Component } from '@angular/core';
+import { Person } from './people.model';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'My first angular project';
+  title = 'People List';
+  people: Person[] = [
+    new Person('Cristian', 'Alvarez'),
+    new Person('Tamara', 'Gomez'),
+  ];
+  inputName: string;
+  inputLastname: string;
+
+  onAddPerson() {
+    let person1 = new Person(this.inputName, this.inputLastname);
+    this.people.push(person1);
+  }
 }
---
+++ src/app/people.model.ts
@@ -0,0 +1,10 @@
+export class Person {
+  /*name:string;
+  lastname:string;
+
+  constructor(name:string, lastname:string){
+    this.name=name;
+    this.lastname=lastname;
+  }*/
+  constructor(public name: string, public lastname: string) {}
+}
