1a8cb4dd549ef280dff0422141f2a9aa400a7e1f
Cristian Alvarez <c.alvarez.r3004@gmail.com>
2020-05-14 14:10:48 -0400

    ViewChild implementation

--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -1,4 +1,11 @@
-import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  Output,
+  EventEmitter,
+  ViewChild,
+  ElementRef,
+} from '@angular/core';
 import { Person } from '../people.model';
 
 @Component({
@@ -8,15 +15,17 @@ import { Person } from '../people.model';
 })
 export class FormComponent implements OnInit {
   @Output() createdPerson = new EventEmitter<Person>();
-  inputName: string;
-  inputLastname: string;
+  // inputName: string;
+  // inputLastname: string;
+  @ViewChild('inputName') inputName: ElementRef;
+  @ViewChild('inputLastname') inputLastname: ElementRef;
 
   constructor() {}
 
   ngOnInit(): void {}
 
   onAddPerson() {
-    let person1 = new Person(this.inputName, this.inputLastname);
+    let person1 = new Person(this.inputName.nativeElement.value,this.inputLastname.nativeElement.value);
     this.createdPerson.emit(person1);
   }
 }
